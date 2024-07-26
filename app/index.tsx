03ba8ff444eafcbcbc389a3f2b4b883b07be9032
jehopgood <jehopgood@gmail.com>
2024-07-26 21:07:50 +0000

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+
+export default function Home() {
+    return (
+        <ThemedView>
+            <ThemedText>
+                Olá Mundo!
+            </ThemedText>
+        </ThemedView>
+    )
+}
